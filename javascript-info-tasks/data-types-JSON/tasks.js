c82ace35fd9c462f461930115af93aeacdd4ccf0
Zenon <zenonpodolak@proton.me>
2025-02-24 16:08:13 +0100

    trying out JSON.stringify with a function as a replacer

--- javascript-info-tasks/data-types-JSON/tasks.js
+++ javascript-info-tasks/data-types-JSON/tasks.js
@@ -6,9 +6,26 @@ let student = {
     isAdmin: true,
     courses: ['js', 'c', 'mathematics'],
     spouse: true,
+    sayHi() {
+        alert('Hi');
+    },
+    details: {
+        hobby: 'baking bread',
+        languages: ['pl', 'en']
+    },
+    occupation: undefined
 };
 
 let studentString = JSON.stringify(student);
 
 console.log(typeof studentString);
 console.log(studentString);
+console.log( JSON.stringify(student, ['name', 'courses']));
+console.log( JSON.stringify(student, function replacer(key, value) {
+    console.log(`${key}: ${value}`);
+    return (key === 'details' ? undefined : value);
+}))
+
+// ok, I see
+
+
