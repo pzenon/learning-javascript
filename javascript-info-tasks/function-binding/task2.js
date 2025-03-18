0539caa8d0134d5a108865ab925f12f44089427a
Zenon <zenonpodolak@proton.me>
2025-03-18 15:19:45 +0100

    chapter completed

---
+++ javascript-info-tasks/function-binding/task2.js
@@ -0,0 +1,18 @@
+// Task
+// Partial application for login
+
+function askPassword(ok, fail) {
+  let password = prompt("Password?", '');
+  if (password == "rockstar") ok();
+  else fail();
+}
+
+let user = {
+  name: 'John',
+
+  login(result) {
+    alert( this.name + (result ? ' logged in' : ' failed to log in') );
+  }
+};
+
+askPassword(() => user.login(true), () => user.login(false));
